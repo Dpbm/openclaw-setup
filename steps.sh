ab4ec4729c5741f45b063562bcb108e571bbbe5b
Dpbm <dpbm316@gmail.com>
2026-03-29 12:28:47 -0300

    updated script

--- steps.sh
+++ steps.sh
@@ -1,16 +1,56 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 export OPENCLAW_IMAGE="ghcr.io/openclaw/openclaw:latest"
+
 export OPENCLAW_HOME="/home/node/.openclaw"
 export LOCAL_FOLDER="./claw"
 
+export LOCAL_SHARED_FOLDER="./shared"
+export SHARED_FOLDER="/home/node/shared"
+
+export USER_CLAW_ID=1000
+export USER_CLAW_GID=1000
+
+export PORT_WS=18789
+
+OPENCLAW_TOKEN="${1:-default}"
+
 #docker pull $OPENCLAW_IMAGE
 
-if [ ! -d $LOCAL_FOLDER ]; then
-  echo "Creating $LOCAL_FOLDER..."
-  mkdir $LOCAL_FOLDER
-  echo "Giving correct ownership..."
-  sudo chown -R 1000:1000 $LOCAL_FOLDER
-fi
+create_folder() {
+  local FOLDER="$1"
 
-docker run -e OPENCLAW_TOKEN="$1" -u "1000:1000" -v "$LOCAL_FOLDER:$OPENCLAW_HOME" -it $OPENCLAW_IMAGE openclaw onboard
+  if [ ! -d "$FOLDER" ]; then
+    echo "--------------------"
+    echo "Creating $FOLDER..."
+    mkdir -p "$FOLDER"
+
+    echo "Giving correct ownership..."
+    sudo chown -R $USER_CLAW_ID:$USER_CLAW_GID "$FOLDER"
+  fi
+
+}
+
+run_command() {
+  local COMMAND="$1"
+  echo "Running docker command: $COMMAND"
+
+  docker run --rm \
+    \
+    --name "openclaw" \
+    -e OPENCLAW_TOKEN="$OPENCLAW_TOKEN" \
+    -u "$USER_CLAW_ID:$USER_CLAW_GID" \
+    -p "$PORT_WS:$PORT_WS" \
+    -v "$LOCAL_FOLDER:$OPENCLAW_HOME" \
+    -v "$LOCAL_SHARED_FOLDER:$SHARED_FOLDER" \
+    -it $OPENCLAW_IMAGE \
+    "$COMMAND" #--network host \
+}
+
+create_folder "$LOCAL_FOLDER"
+create_folder "$LOCAL_SHARED_FOLDER"
+
+# run_command "openclaw onboard"
+run_command "/bin/sh"
